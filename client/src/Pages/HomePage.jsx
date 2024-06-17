70415787c4697773d8ad7cd7724ee1bfce4be344
Roland Silva <roland.silva@me.com>
2024-06-17 15:04:49 -0400

    T3 Capstone Project wip

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Homepage.css";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
@@ -13,16 +14,34 @@ const HomePage = () => {
             />
           </div>
           <div className="searchcontainer">
-            <div style={{display: "flex", backgroundColor:"lightblue", border:"1px solid black", padding:"4px", marginLeft:"30px"}}>
+            <div
+              style={{
+                display: "flex",
+                backgroundColor: "lightblue",
+                border: "1px solid black",
+                padding: "4px",
+                marginLeft: "30px",
+              }}
+            >
               <h2 className="searchtext">
                 Search by Category (click image below)
               </h2>
-              <label style={{fontSize:"25px", padding:"0px 10px", fontWeight:"bold"}}>Search by Product </label>
+              <label
+                style={{
+                  fontSize: "25px",
+                  padding: "0px 10px",
+                  fontWeight: "bold",
+                }}
+              >
+                Search by Product{" "}
+              </label>
               <input />
             </div>
             <div className="productimgcontainer">
-            <img src="./airpods _1_png.png" id="airpods"/>
-            <img src="./airtag_single_png.png" id="airtags"/>
+              {/* <Link to="/productairpods"> */}
+                <img src="./airpods _1_png.png" id="airpods" />
+              {/* </Link> */}
+              <img src="./airtag_single_png.png" id="airtags" />
             </div>
           </div>
         </div>
