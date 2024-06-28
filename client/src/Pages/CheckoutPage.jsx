c8d3f4724342187fef28c82e21527585e6b955f9
Tim Quattrochi <timq82@gmail.com>
2024-06-28 14:20:15 -0400

    WIP checkout page

--- client/src/Pages/CheckoutPage.jsx
+++ client/src/Pages/CheckoutPage.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { toast } from "react-toastify";
-import useProvideCart from "../hooks/useCart"
+import useProvideCart from "../hooks/useCart";
 // import { createOrder } from "../utils/axiosService";
-import CheckoutForm from "../components/CheckoutForm/index"
-import ErrorBoundary  from "../components/ErrorBoundary";
+import CheckoutForm from "../components/CheckoutForm/index";
+import ErrorBoundary from "../components/ErrorBoundary";
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner";
 import api from "../utils/api.utils";
@@ -17,12 +17,12 @@ const initialState = {
 
 function CheckoutPage() {
   const [data, setData] = useState(initialState);
-  const { state, resetCart, calculateCartTotal, createOrder } = useProvideCart();
-  const [orderId, setOrderId] = useState("")
-
+  const { state, resetCart, calculateCartTotal, createOrder } =
+    useProvideCart();
+  const [orderSummary, setOrderSummary] = useState(null);
 
   const placeOrder = async (orderFormData) => {
-    console.log(orderFormData)
+    console.log(orderFormData);
     let orderData = {
       customerDetails: orderFormData,
       items: state.cart,
@@ -36,10 +36,14 @@ function CheckoutPage() {
     });
     try {
       const orderConfirmation = await api.post("/orders", orderData);
-      console.log("order response", orderConfirmation)
+      console.log("order response", orderConfirmation);
       // const orderConfirmation = await createOrder(orderData);
-      toast(`Order Placed Successfully, ${orderConfirmation.data.orderId})`);
-      setOrderId(orderConfirmation.data.orderId)
+      toast(
+        `Order Placed Successfully, ${orderConfirmation.data.orderId})`
+      );
+
+      // TODO: Check response and make sure the data is correct for order summary.
+      setOrderSummary(orderConfirmation.data.orderData.orderContents);
       resetCart();
       setData({
         isSubmitting: false,
@@ -70,10 +74,12 @@ function CheckoutPage() {
           marginTop: "45px",
         }}
       > */}
-        <h2 style={{ marginTop: "20px" }}>Order Confirmation Page</h2>
+      <h2 style={{ marginTop: "20px" }}>Order Confirmation Page</h2>
       {/* </Container> */}
       <ErrorBoundary>
-        {data.errorMessage && <p className="form-error">{data.errorMessage}</p>}
+        {data.errorMessage && (
+          <p className="form-error">{data.errorMessage}</p>
+        )}
         {state.itemCount && !data.isConfirmed ? (
           <CheckoutForm placeOrder={placeOrder} />
         ) : (
@@ -88,13 +94,24 @@ function CheckoutPage() {
                   }}
                 >
                   {`Your order is confirmed! 
-                  ${orderId}` }
-
+                  ${
+                    orderSummary && (
+                      <div>
+                        Your Order Number is:
+                        {JSON.stringify(orderSummary)}
+                        <br />
+                        Your Order Total is: $
+                        {orderSummary.orderTotal}
+                      </div>
+                    )
+                  }`}
                 </p>
               )}
 
               <div className="col-sm-12 d-flex justify-content-center">
-                <p>You'll receive confirmation in your email shortly.</p>
+                <p>
+                  You'll receive confirmation in your email shortly.
+                </p>
               </div>
               <div className="col-sm-12 d-flex justify-content-center">
                 <Link to="/">Continue shopping!</Link>
