bd8d53666614e22e9f81d39c8323b7baf0303ff5
Roland Silva <roland.silva@me.com>
2024-07-01 21:52:08 -0400

    T3 Capstone Project account account-order detail pages

--- client/src/Pages/AccountOrdersPage.jsx
+++ client/src/Pages/AccountOrdersPage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import { useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
-// import { useAuth } from "../../hooks/useAuth";
-// import { useProvideAuth } from "../../hooks/useAuth";
+import { useAuth } from "../hooks/useAuth";
+import { useProvideAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 // import { toast } from "react-toastify";
 import api from "../utils/api.utils";
@@ -23,79 +23,36 @@ const AccountOrdersPage = () => {
   );
 
   const [orders, setOrders] = useState(0);
-  const [orderHistory, setOrderHistory] = useState(0);
+  const [orderDetails, setOrderDetails] = useState(0);
   const [loading, setLoading] = useState(0);
   const [error, setError] = useState(0);
 
   const location = useLocation();
   const { order } = location.state || {}; // Destructure the state to get the product data
-  const { _id } = useParams();
-  console.log(location)
+  const { _id: orderId } = useParams();
+  // console.log(location);
 
-  // const auth = useAuth();
+  const auth = useAuth();
   const navigate = useNavigate();
 
-  // const { updatePassword } = useProvideAuth();
+  const { updatePassword } = useProvideAuth();
 
-  // object destructuring user from auth.state from our
-  // useAuth hook that is inside our useProvideAuth hook
-  //in the hooks/useAuth.js file
+  const {
+    user: {
+      _id,
+      customerFirstName,
+      customerLastName,
+      customerAddress1,
+      customerAddress2,
+      customerCity,
+      customerState,
+      customerZip,
+      customerEmail,
+      customerPhone,
+    },
+  } = auth.state;
 
-  //   ******   needs to be added back in  **********
-  // const {
-  //   user: {
-  //     _id,
-  //     customerFirstName,
-  //     customerLastName,
-  //     customerAddress1,
-  //     customerAddress2,
-  //     customerCity,
-  //     customerState,
-  //     customerZip,
-  //     customerEmail,
-  //     customerPhone,
-  //   },
-  // } = auth.state;
-  // ********* add above back in ***********
 
-  // const handleChangePassword = async (e) => {
-  //   e.preventDefault();
-
-  //   setPasswordFormData((prev) => ({
-  //     ...prev,
-  //     loading: true,
-  //     passwordError: null,
-  //   }));
-
-  //   const { currentPassword, password, confirmPassword } = passwordFormData;
-
-  //   try {
-  //     await updatePassword(currentPassword, password, confirmPassword);
-  //     setPasswordFormData(initialPasswordFormData);
-  //     toast.success("Password changed successfully");
-  //   } catch (error) {
-  //     console.log(error.message);
-  //     toast.error(error.message);
-
-  //     // If the user is forbidden or unauthorized, it could be an expired JWT token. Redirect to the login page.
-  //     if (error.message === "Forbidden" || error.message === "Unauthorized") {
-  //       navigate("/login");
-  //     }
-  //   } finally {
-  //     setPasswordFormData((prev) => ({
-  //       ...prev,
-  //       loading: false,
-  //     }));
-  //   }
-  // };
-
-  // const handleInputChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setPasswordFormData((prev) => ({
-  //     ...prev,
-  //     [name]: value,
-  //   }));
-  // };
 
   useEffect(() => {
     const getOrders = async () => {
@@ -115,26 +72,18 @@ const AccountOrdersPage = () => {
   }, []);
 
   useEffect(() => {
-    const singleOrder = order && orders.filter((item) => {
-      return item._id === _id
-    })
-    console.log(order)
-    console.log(singleOrder)
-    setOrderHistory(singleOrder);
-  }, [order, _id]);
-
-  // const renderOrdersArray = (items) => {
-  //   console.log("items: ", items)
-  //   const orderItemsList = items.map((i) => (
-  //     i._id, i.createdAt, i.orderTotal
-  // ));
-  //   console.log("orderItemsList", orderItemsList)
-  //   return orderItemsList;
-  // };
+    const singleOrder =
+      orders &&
+      orders.filter((item) => {
+        return item._id === orderId;
+      });
+    // console.log(order);
+    // console.log(singleOrder);
+    setOrderDetails(singleOrder[0]);
+    console.log(orderDetails);
+  }, [orders, order, orderId]);
 
   const renderOrdersArray = (items) => {
-    console.log("items: ", items);
-
     const orderItemsList = items.map(({ _id, createdAt, orderTotal }) => (
       <li key={_id}>
         <p>Order ID: {_id}</p>
@@ -142,24 +91,6 @@ const AccountOrdersPage = () => {
         <p>Total: ${orderTotal.toFixed(2)}</p>
       </li>
     ));
-
-    // *****  orders model  ********
-    // {
-    //   id: {type: Number,// required: true,},
-    //   orderNumber: {type: Number,// required: true,},
-
-    //   orderContents: [
-    //     {
-    //       productNbr: {type: String,required: true},
-    //       itemQuantity: {type: Number,required: true,},
-    //     },
-    //   ],
-    //   couponRate: {type: Number,required: true,},
-    //   orderTotal: {type: Number,required: true,},
-    //   shippingTime: {type: Number,required: true,},
-    //   shippingDate: {type: Date,required: true,},
-    // },
-    // { timestamps: true }
 
     console.log("orderItemsList", orderItemsList);
 
@@ -170,30 +101,20 @@ const AccountOrdersPage = () => {
     return <div>Loading...</div>;
   }
   // TODO: Make a form component to handle the login, register, and password change forms. This can be a stretch goal.
+
   return (
     <>
       <div className="account-container">
-        <section className="account-info-section">
-          <div className="info-title">Account Information</div>
-          <div className="section-contents">
-            <p>Account Number: 1234</p>
-            <p>First Name: Tim</p>
-            <p>Last Name: Quattrochi</p>
-            <p>Email Address: Tim1234@Kenzie.com</p>
-            <p>Phone Number: 123-456-7890</p>
-          </div>
-        </section>
+  
 
-        {/* 
-//  ********* actual needed account data ****** */}
-        {/* <section className="account-info">
+        <section className="account-info">
           <div className="info-title">User Account Information</div>
           <p>Account Number: {_id}</p>
           <p>First Name: {customerFirstName}</p>
           <p>Last Name: {customerLastName}</p>
           <p>Email Address: {customerEmail}</p>
           <p>Phone Number: {customerPhone}</p>
-        </section> */}
+        </section>
 
         <section className="middle-info-section">
           <div className="middle-info-section-image">
@@ -241,34 +162,28 @@ const AccountOrdersPage = () => {
 
         <section className="orders-placed-section">
           <div className="info-title">Order Details</div>
-
-          {/* <div className="ordersplacedtitle">Order Details</div> */}
           <div className="orderinfo">
             <div className="section-contents">
-              <p>Order Number: {_id }</p>
-              <p>Order Number: 1234</p>
-              <p>Order Date: 01/02/24</p>
-              <p>---------------</p>
-              {/* <p>Product Image url</p> */}
-              <img src="/M3mbpmax.png" />
-              {/* <img src={product.image_url} className="imgDiv" alt={product.name} /> */}
-              <p>Product Name: M3 MacBook Max</p>
-              <p>Product Category: MacBooks</p>
-              <p>Item quantity: 2</p>
-              <p>---------------</p>
-              <img src="/iwatchultra.png" />
-              <p>Product Name: iWatch Ultra 2</p>
-              <p>Product Category: iWatches</p>
-              <p>Item quantity: 1</p>
-              <p>---------------</p>
-              <img src="/ipadProM4.png" />
-              <p>Product Name: M4 iPad Pro 13in</p>
-              <p>Product Category: iPads</p>
-              <p>Item quantity: 4</p>
-              <p>---------------</p>
-              <p>Order Total: $ 13,999</p>
+              {orderDetails && (
+                <>
+                  <p>Order Number: {orderId}</p>
+                  <p>Order Date: {orderDetails.createdAt}</p>
+                  {orderDetails.orderContents.map((item) => (
+                    <div key={item._id}>
+                      <p>---------------</p>
+                      {/* <p>Product Image url</p> */}
+                      <img src={`${item.image_url}`} />
+                      <p>Product Name: {item.name}</p>
+                      <p>Product Category: {item.category}</p>
+                      <p>Item quantity: {item.itemQuantity}</p>
+                    </div>
+                  ))}
+
+                  <p>---------------</p>
+                  <p>Order Total: $ {orderDetails.orderTotal}</p>
+                </>
+              )}
             </div>
-            {/* {orders && renderOrdersArray(orders)} */}
           </div>
         </section>
       </div>
