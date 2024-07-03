06db68b2c60f53ab443897e85c67f03d9487e73a
Roland Silva <roland.silva@me.com>
2024-07-03 11:55:04 -0400

    T3 Capstone Project cat prod search page

--- client/src/Pages/CatFilterBySearchPage.jsx
+++ client/src/Pages/CatFilterBySearchPage.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import "./CatFilterBySearchPage.css"
+import React from "react";
+import "./CatFilterBySearchPage.css";
 
 const CatFilterBySearchPage = () => {
-        return (
-          <div className="coming-soon-page">
-              <div className="containerdivWrapper">
-                <div className="containerdiv">The individual search links are coming soon !</div>
-                <p>Thank you for your patience.</p>
-              </div>
+  return (
+    <div class="coming-soon-wrapper">
+      <div className="coming-soon-page">
+        <div className=".orders-placed-section">
+          <div className="containerdiv">
+            The individual search links are coming soon !
           </div>
-        );
-      };
+          <p>Thank you for your patience.</p>
+          <img src="./smiley_PNG27.png" alt="Smiley Face" className="logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CatFilterBySearchPage
+export default CatFilterBySearchPage;
