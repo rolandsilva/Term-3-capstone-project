cc77b4a5b51f254005b7a26a0407824164dfeba1
Roland Silva <roland.silva@me.com>
2024-07-11 14:36:01 -0400

    T3 Capstone Project deploy

--- server/src/app.js
+++ server/src/app.js
@@ -18,6 +18,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(API_URL, router);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../client/dist")))
+  app.all("*", (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, "../../client/dist/index.html"))
+  })
+}
 
 app.listen(PORT, () =>
   console.log(`[Server] Listening for requests at http://localhost:${PORT}`)
